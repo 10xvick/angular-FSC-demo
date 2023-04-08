936254ad1e8ca8fa651d14e2230e0c87f578e86a
10xvick <103027108+10xvick@users.noreply.github.com>
2023-04-08 15:03:55 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
 import { AppService } from './app.service';
 
 @Component({
@@ -8,23 +7,12 @@ import { AppService } from './app.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(private service: AppService) {
-    this.service.refresh$.next();
-  }
-  tableData:Observable<any[]>
-
-  private tableCols:string[];
-  ngOnInit() {
-    
-  }
+  constructor(private service: AppService) {}
 
   tabs = ['Pending', 'Whitelisted', 'Rejected'];
   activeTab = 'Pending';
 
-
-  // tableData = Array(10).fill(this.tableCols);
-
-  popup = {
+  popup: any = {
     show: true,
     toggle: (flag?) => {
       if (flag != undefined) {
@@ -37,12 +25,8 @@ export class AppComponent {
   popuptoggle = (flag?) => {
     if (flag != undefined) {
       this.popupshow = flag;
+      this.popup.show = flag;
+      console.log(this.popup.show);
     } else this.popupshow = !this.popupshow;
   };
 }
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,38 +1,48 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, interval, Observable, Subject, Subscriber, Subscription, timer } from 'rxjs';
-import { map, startWith, switchMap, takeUntil } from 'rxjs/operators';
+import { BehaviorSubject, interval, Observable, of } from 'rxjs';
+import { map, startWith, switchMap, takeUntil , delay} from 'rxjs/operators';
 
 @Injectable()
 export class AppService {
-  constructor(private http: HttpClient) {
-    const cols = [
-      0,
-      'User Name',
-      "Father's Name",
-      'Employee ID',
-      'Category',
-      'Circle',
-      'Supervisor',
-    ];
-    this.data = Array(10).fill(cols)
-  };
-  data:any[];
-
+  constructor(private http: HttpClient) {};
 
   private dummy:Observable<any[]> = new Observable(Subscriber=>{
-    Subscriber.next(this.data);
+    const sample = {
+      "fileId": "123456",
+      "requestID" : "654321",
+      "distributorNo" : "9896968787",
+      "userNo": "9876765656",
+      "usertype": "Acquisition BDE",
+      "userCategory": "Ranger",
+      "userName": "Arun Kumar",
+      "fatherName": "Om Prakash",
+      "DOB": "01/12/1992",
+      "DOJ": "07/04/2023",
+      "interviewerNo": "9785858585",
+      "interviewerOLM": "213454",
+      "interviewDate": "07/04/23",
+      "access": ["Ranger","Mitra"],
+      "status": "pending",
+      "remark": "Successfully Raised",
+      "remarkType": "Auto"
+    }
+    const cols = Object.keys(sample);
+    const data = Array(10).fill(sample);
+    Subscriber.next({cols:cols,data:data});
   });
 
   
-
+  /** refresh subject | call Next() to trigger */
   public refresh$ = new BehaviorSubject<void>(null);
+  /** table data observable | use with async pipe */
   public tableData = this.refresh$.pipe(
     switchMap(() => interval(120000/30).pipe(
       startWith(0),
       switchMap((e) => { 
         console.log('refresh')
         return this.dummy || this.http.get<any[]>('/api/data')}),
+      takeUntil( of(1).pipe(delay(5000)) )
     )),
   );
 
