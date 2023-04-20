de137f33ba305ea5deaa42bddb4ab91417b58a68
10xvick <103027108+10xvick@users.noreply.github.com>
2023-04-20 11:25:09 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,24 +12,8 @@ export class AppComponent {
   tabs = ['Whitelisting Requests', 'Ranger Details', 'Upload Rangers'];
   activeTab = this.tabs[0];
 
-  popup: any = {
-    show: true,
-    toggle: (flag?) => {
-      if (flag != undefined) {
-        this.popup.show = flag;
-      } else this.popup.show = !this.popup.show;
-    },
-  };
-
-  popupshow = false;
-  popuptoggle = (flag?) => {
-    if (flag != undefined) {
-      this.popupshow = flag;
-      this.popup.show = flag;
-      console.log(this.popup.show);
-    } else this.popupshow = !this.popupshow;
-  };
-
-  pages:number[] = Array(10).fill(0).map((e,i)=>i+1);
+  details = null;
+  setDetails(data) {
+    this.details = data;
+  }
 }
-
